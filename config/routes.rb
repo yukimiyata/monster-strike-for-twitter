cfbaff7ddb6a793f83f1719ad3afb9a5dc36f07b
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-07 13:12:04 +0900

    add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+  resources :users, only: %i[new create]
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 end
