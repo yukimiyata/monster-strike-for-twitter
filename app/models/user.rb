5fe49c1632e1295e4c9b5ed3ccaa4aea51c0414f
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-07 14:22:44 +0900

    add: recruitモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
   validates :email, uniqueness: true, presence: true
+
+  has_many :posts
 end
