fa6ddbaab4cf00cb40888605682055360d213357
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-08 11:16:54 +0900

    add: メンバー募集コントローラーの追加とpostコントローラーの編集

--- app/controllers/recruiting_positions_controller.rb
+++ app/controllers/recruiting_positions_controller.rb
@@ -1,23 +1,35 @@
 class RecruitingPositionsController < ApplicationController
   before_action :require_login
-  # def create
-  #   post = current_user.posts.find(session[:post_id])
-  #   post.member_count.times do |index|
-  #     character = character_params[index][:character]
-  #     description = description_params[index][:description]
-  #     recruit = post.recruits.build(character: character, description: description)
-  #     recruit.save
-  #   end
-  #   redirect_to post_path(post.id), success: '投稿しました'
-  # end
+
+  def new
+    @post = Post.find(params[:format])
+    session[:post_id] = @post.id
+    @post.member_capacity.times { @recruiting_position = @post.recruiting_positions.new }
+  end
+
+  def create
+    post = current_user.posts.find(session[:post_id])
+    post.member_capacity.times do |index|
+      character = character_params[index][:character]
+      description = description_params[index][:description]
+      recruiting_position = post.recruiting_positions.build(character: character, description: description)
+      if recruiting_position.save
+        continue
+      else
+        flash.now[:danger] = '登録出来ませんでした'
+        render :new
+      end
+    end
+    redirect_to post_path(post.id), success: '投稿しました'
+  end
 
   private
 
   def character_params
-    params.require(:recruit).permit(recruits: :character)[:recruits]
+    params.require(:recruiting_position).permit(recruiting_positions: :character)[:recruiting_positions]
   end
 
   def description_params
-    params.require(:recruit).permit(recruits: :description)[:recruits]
+    params.require(:recruiting_position).permit(recruiting_positions: :description)[:recruiting_positions]
   end
 end
