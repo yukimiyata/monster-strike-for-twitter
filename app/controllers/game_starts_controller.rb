8a78507457f407965970a0dfa7e59701c7f63f36
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-09 02:10:03 +0900

    add: ゲームをスタートするためのコントローラーを追加

---
+++ app/controllers/game_starts_controller.rb
@@ -0,0 +1,25 @@
+class GameStartsController < ApplicationController
+  before_action :set_post, only: :new
+  before_action :post_user_valid?, only: :new
+
+  def new
+    @joined_users = @post.recruiting_positions.map{ |k| k.joined_user }
+    @post.status = :started
+    @post.save!
+  end
+
+  def show
+    post = Post.find(params[:id])
+    redirect_to post.invite_url
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:format])
+  end
+
+  def post_user_valid?
+    render 'posts#show' if current_user.id != @post.user_id
+  end
+end
