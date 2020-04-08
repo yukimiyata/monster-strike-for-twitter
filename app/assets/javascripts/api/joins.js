07d0e1017576b9d21e5393fd643cc6676bbfdcf5
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-09 06:58:31 +0900

    add: 自動で参加者が更新されるように修正

--- app/assets/javascripts/api/joins.js
+++ app/assets/javascripts/api/joins.js
@@ -1,26 +1,54 @@
-window.addEventListener('load', function() {
-    let post_number = location.href.split("/")[location.href.split("/").length - 1];
-    setInterval(() => {
-        $.ajax({
-            url: "/api/joins",
-            type: "GET",
-            data: { id: post_number },
-            dataType: "json"
-        }).done(function(joins){
-            let joins_result = joins.joins_sum;
-            for(let i = 0; i < joins_result.length; i++){
-                let recruit_id = joins_result[i][0];
-                let recruit_tag = "recruit-number-" + recruit_id;
-                let result_tag = document.getElementById(recruit_tag);
-                result_tag.textContent = joins_result[i][1];
-                if (joins_result[i][2]) {
-                    let start_tag = "start-game-" + recruit_id;
-                    let start_button = document.getElementById(start_tag);
-                    start_button.href = "/judges/" + joins.post_id;
-                    start_button.textContent = "出撃する！";
+let timer;
+// 募集主が１時間で停止
+let counter = 1800;
+
+window.addEventListener('load', function () {
+    timer = setInterval("statusAjax()", 2000);
+});
+
+function statusAjax() {
+    let postNumber = location.href.split("/")[location.href.split("/").length - 1];
+    $.ajax({
+        url: "/api/joins",
+        type: "GET",
+        data: {id: postNumber},
+        dataType: "json"
+    }).done(function (joins) {
+        let joinedInfo = joins.joins_info;
+        let postStatus = joins.status;
+        let userId = joins.user_id;
+        let postUserId = joins.post_user_id;
+        let recruitingIdName, recruitingTag, gameStartIdName, gameStartTag, gameCloseIdName,
+            game_close_tag;
+        for (let i = 0; i < joinedInfo.length; i++) {
+            if (joinedInfo[i][1]) {
+                recruitingIdName = "recruiting-position-name-" + joinedInfo[i][0];
+                recruitingTag = document.getElementById(recruitingIdName);
+                recruitingTag.textContent = joinedInfo[i][1] + "さん";
+                if (postStatus) {
+                    if (userId == joinedInfo[i][2]) {
+                        gameStartIdName = "game-start-" + joinedInfo[i][0];
+                        gameStartTag = document.getElementById(gameStartIdName);
+                        gameStartTag.textContent = "ゲームを開始する";
+                        gameStartTag.href = '/game_starts/' + postNumber;
+                    }
+                }
+            } else {
+                if (postStatus) {
+                    if (userId != postUserId) {
+                        gameCloseIdName = "game-close-" + joinedInfo[i][0];
+                        game_close_tag = document.getElementById(gameCloseIdName);
+                        game_close_tag.textContent = '募集を締め切りました';
+                        game_close_tag.href = "#";
+                    }
                 }
             }
-        }).fail(function (joins) {
-        });
-    }, 2000);
-});
+        }
+    }).fail(function (joins) {
+    });
+
+    counter--;
+    if(counter == 0){
+        clearInterval(timer);
+    }
+}
