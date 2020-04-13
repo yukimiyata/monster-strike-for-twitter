72e5104dd7cba5f7a7312235c79e9afa0e2e1a8a
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-13 20:56:45 +0900

    fix: 定数で定義するように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,13 @@ class Post < ApplicationRecord
   scope :recently, -> { where('created_at > ?', 10.hours.ago) }
 
   enum status: { waiting: 0, started: 1 }
+  MEMBER_CAPACITY_BASE = 1
 
   def process_api_attributes(body)
-    member_capacity_base = 1
     begin
       quest_name_base = body[:body].split(/[「|」]/)[1]
       url_base = "monsterstrike-app://joingame/?join=" + body[:body].split(/[「|」]/)[2].split("?pass_code=")[1].split(/[\r | \n]/).first
-      { quest_name: quest_name_base, invite_url: url_base, member_capacity: member_capacity_base }
+      { quest_name: quest_name_base, invite_url: url_base, member_capacity: MEMBER_CAPACITY_BASE }
     rescue
       { quest_name: nil, invite_url: nil, member_capacity: nil }
     end
