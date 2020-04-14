71cfa0df7df2d891849277722ed6f6baa6ce2b9f
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-14 16:20:28 +0900

    fix: freeze対応

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,18 @@ class Post < ApplicationRecord
   scope :recently, -> { where('created_at > ?', 10.hours.ago) }
 
   enum status: { waiting: 0, started: 1 }
-  MEMBER_CAPACITY_BASE = 1
+
+  module MyConstant
+    MEMBER_CAPACITY_BASE = 1
+    MEMBER_CAPACITY_BASE.freeze
+  end
+  MyConstant.freeze
 
   def process_api_attributes(body)
     begin
       quest_name_base = body[:body].split(/[「|」]/)[1]
-      url_base = "monsterstrike-app://joingame/?join=" + body[:body].split(/[「|」]/)[2].split("?pass_code=")[1].split(/[\r | \n]/).first
-      { quest_name: quest_name_base, invite_url: url_base, member_capacity: MEMBER_CAPACITY_BASE }
+      url_base = "monsterstrike-app://joingame/?join=" + body[:body].split(/[「|」]/)[2].split("?pass_code=")[1].split(/[\r|\n]/).first
+      { quest_name: quest_name_base, invite_url: url_base, member_capacity: MyConstant::MEMBER_CAPACITY_BASE }
     rescue
       { quest_name: nil, invite_url: nil, member_capacity: nil }
     end
