7cc406f1ca21a03618d2b9328940ea001e3d5e9c
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-14 16:20:54 +0900

    add: エラー時の挙動追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
+  unless Rails.env.development?
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from StandardError, with: :render_500
+  end
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, danger: 'ログインしてください'
+  end
+
+  def render_404
+    render file: Rails.root.join('public/404.html'),
+           status: 404,
+           layout: false,
+           content_type: 'text/html'
+  end
+
+  def render_500(error_e)
+    if error_e
+      logger.error error_e.backtrace
+    end
+
+    render file: Rails.root.join('public/500.html'),
+           status: 500,
+           layout: false,
+           content_type: 'text/html'
+  end
 end
