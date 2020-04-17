8312b4e6bee0dec3bea1573611467ec15d23d07e
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-17 21:33:17 +0900

    add: ユーザー編集画面と、ユーザーのgame_nameのupdateを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %w[new create]
+  before_action :set_user, only: %w[edit update]
+  before_action :logged_in_users_page?, only: %w[edit update]
 
   def new
     @user = User.new
   end
+
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -15,9 +19,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user.game_name = game_name_params[:game_name]
+    @user.save
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def logged_in_users_page?
+    redirect_to root_path unless @user == current_user
+  end
+
+  def game_name_params
+    params.require(:user).permit(:game_name)
   end
 end
