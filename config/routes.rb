71448ef7557f4aff182547ca2aaae50866304aa4
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-17 21:38:35 +0900

    fix: ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :users, only: %i[new create] do
+  resources :users, only: %i[new create edit update] do
     member do
       get :following, :followers
     end
