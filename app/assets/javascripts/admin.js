34cd31fe99bdf12fa4464f34efaf1bd6a58a0296
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-21 01:38:17 +0900

    add: admin-lte用のマニフェストファイルを追加

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,8 @@
+//= require jquery3
+//= require rails-ujs
+//= require activestorage
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery/dist/jquery.min.js
+//= require bootstrap/dist/js/bootstrap.bundle.min.js
+//= require admin-lte/dist/js/adminlte.min
