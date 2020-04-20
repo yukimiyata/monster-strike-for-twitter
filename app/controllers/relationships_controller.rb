deef68eb840ca9e09bf9f51f7fa772252eca2955
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-21 05:17:59 +0900

    fix: comment

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
 
   def index
     follower_relationships = Relationship.where(followed_id: current_user.id)
-    # ↓　[] << nil が発生した。
+    # ↓　[] << nil が発生した
     # @posts = follower_relationships.inject([]) { |posts, id| posts << id.follower.latest_post if id.follower.latest_post.present? && id.follower.latest_post.waiting? }
     @posts = follower_relationships.map { |id| id.follower.latest_post if id.follower.latest_post.present? && id.follower.latest_post.waiting? }.compact
     @posts = Kaminari.paginate_array(@posts).page(params[:page]) if @posts.present?
