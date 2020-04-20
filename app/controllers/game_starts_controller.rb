29ec9a367af4fab6ba5c35b6f386a4bd4ae58bd8
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-20 17:21:56 +0900

    fix: ゲームスタート用のbefore_actionを整備

--- app/controllers/game_starts_controller.rb
+++ app/controllers/game_starts_controller.rb
@@ -1,5 +1,7 @@
 class GameStartsController < ApplicationController
   before_action :set_post, only: %w[show starting]
+  before_action :post_owner?, only: %w[show]
+  before_action :allowed_user_to_start?, only: %w[starting]
 
   def show
     @joined_users = @post.recruiting_positions.map(&:joined_user)
@@ -8,13 +10,20 @@ class GameStartsController < ApplicationController
   end
 
   def starting
-    post = Post.find(params[:id])
-    redirect_to post.invite_url
+    redirect_to @post.invite_url
   end
 
   private
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_owner?
+    redirect_to root_path unless @post.user_id == current_user.id
+  end
+
+  def allowed_user_to_start?
+    redirect_to root_path, danger: '入室が許可されていません' unless @post.include_joined?(current_user)
   end
 end
