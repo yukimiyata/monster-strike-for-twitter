4c4716fcfd15ee309b8c68a99cba012b94072978
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-22 04:17:53 +0900

    add: avatarのアップローダーを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :joined_user
+  mount_uploader :avatar, AvatarUploader
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
