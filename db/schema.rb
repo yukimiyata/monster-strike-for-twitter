58552258c8e274966a98b895597cfe86bb2cf278
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-22 04:18:17 +0900

    add: カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_103758) do
+ActiveRecord::Schema.define(version: 2020_04_21_182709) do
 
   create_table "blacklists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_103758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
