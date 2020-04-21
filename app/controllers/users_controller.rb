4765fd630b10eeebf2f9ba9fa14ee9096bc57abe
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-22 04:13:18 +0900

    fix: userコントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.game_name = game_name_params[:game_name]
+    @user.assign_attributes(user_params)
     if @user.save
       render :update
     else
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :game_name, :password, :password_confirmation, :avatar)
   end
 
   def set_user
@@ -40,9 +40,5 @@ class UsersController < ApplicationController
 
   def correct_users_page?
     redirect_to root_path unless @user == current_user
-  end
-
-  def game_name_params
-    params.require(:user).permit(:game_name)
   end
 end
