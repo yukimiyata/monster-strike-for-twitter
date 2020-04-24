b7f2f566d454456cd3529171fe761ca40a5e5b8e
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-25 02:57:17 +0900

    fix: deploy.rbとgemfileを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,9 @@ set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{shared_path}/log/puma.access.log"
 set :puma_preload_app, true
+set :bundle_flags,      '--quiet' # this unsets --deployment, see details in config_bundler task details
+set :bundle_path,       nil
+set :bundle_without,    nil
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -29,6 +32,17 @@ namespace :puma do
 end
 
 namespace :deploy do
+  desc 'Config bundler'
+  task :config_bundler do
+    on roles(/.*/) do
+      within release_path do
+        execute :bundle, :config, '--local deployment true'
+        execute :bundle, :config, '--local without "development test"'
+        execute :bundle, :config, "--local path #{shared_path.join('bundle')}"
+      end
+    end
+  end
+
   desc 'upload important files'
   task :upload do
     on roles(:app) do
@@ -59,40 +73,4 @@ end
 
 after 'deploy:published', 'nginx:restart'
 before 'deploy:migrate', 'deploy:db_create'
-
-# config valid only for current version of Capistrano
-# lock "3.9.0"
-#
-# set :application, "my_app_name"
-# set :repo_url, "git@example.com:me/my_repo.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+before 'bundler:install', 'deploy:config_bundler'
