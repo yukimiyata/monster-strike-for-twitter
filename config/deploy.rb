9a77d5630f88c5f2f43ff608c90fa520ffc4ce26
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-25 06:24:07 +0900

    fix: migrateに修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,7 +62,7 @@ namespace :deploy do
     on roles(:db) do
       with rails_env: fetch(:rails_env) do
         within release_path do
-          execute :bundle, :exec, :rake, 'db:create'
+          execute :bundle, :exec, :rake, 'db:migrate'
         end
       end
     end
