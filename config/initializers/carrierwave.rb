7376a6d0343a386c7ab2ddea799474017fbd42d1
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-26 04:15:37 +0900

    fix: carrierwave.rb内の記述を修正と、暗号化の追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,18 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: Settings.aws[:access_key],
         aws_secret_access_key: Settings.aws[:secret_access_key],
-        region: 'ap-northeast-1'
+        region: Settings.aws[:region]
     }
 
     config.fog_directory  = Settings.aws[:s3_bucket]
+    config.fog_provider = 'fog/aws'
     config.cache_storage = :fog
   end
 end
