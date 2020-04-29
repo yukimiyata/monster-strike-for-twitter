63ea7fb46d9b6b97b19140ea4aa177017aa3e3e2
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-04-29 17:47:33 +0900

    fix: バリデーションとアソシエーションを追加

--- app/models/recruiting_position.rb
+++ app/models/recruiting_position.rb
@@ -1,4 +1,7 @@
 class RecruitingPosition < ApplicationRecord
   belongs_to :post
   has_one :joined_user, dependent: :destroy
+
+  validates :character, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 }
 end
