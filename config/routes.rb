0ab2a56ed4d88e21f829e85d5d6b440ec540791b
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-05-06 02:04:35 +0900

    fix: ルーティングを整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,37 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  post "oauth/callback", to: "oauths#callback"
+  post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
-  get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
-  resources :users, only: %i[new create edit update] do
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
+  resources :users, only: %w[edit update] do
     member do
       get :following, :followers
     end
   end
-    get '/login', to: 'user_sessions#new'
-    post '/login', to: 'user_sessions#create'
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create' if Rails.env.production?
   delete '/logout', to: 'user_sessions#destroy'
-  resources :posts
-  resources :joined_users
+  resources :posts, only: %w[index show new create]
+  resources :joined_users, only: %w[create destroy]
   get '/now_join', to: 'joined_users#now_join'
-  resources :game_starts do
+
+  resources :game_starts, only: :show do
     member do
       get 'starting'
     end
   end
+
   namespace :api do
-    resources :joins
-    resources :posts
+    resources :joins, only: :index
+    resources :posts, only: :index
   end
   resources :relationships, only: %w[index create destroy]
   resources :blacklists, only: %w[create destroy]
+
+  get '/terms', to: 'static_pages#terms'
+  get '/explanation', to: 'static_pages#explanation'
+
   namespace :admin do
     root 'dashboards#top'
     get '/login', to: 'user_sessions#new'
