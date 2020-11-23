b01c36ab79ab69cd8151a4a94ab46eeb8052d0e4
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-11-23 18:55:46 +0900

    add: テストユーザーログイン機能を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %w[new create]
+  skip_before_action :require_login, only: %w[new create test_user_1 test_user_2]
 
   def new
     @user = User.new
@@ -13,6 +13,16 @@ class UserSessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def test_user_1
+    @user = login(Rails.application.credentials.test_user[:test_user_1][:email], Rails.application.credentials.test_user[:test_user_1][:password])
+    redirect_to root_path, success: 'テストユーザー１でログインしました'
+  end
+
+  def test_user_2
+    @user = login(Rails.application.credentials.test_user[:test_user_2][:email], Rails.application.credentials.test_user[:test_user_2][:password])
+    redirect_to root_path, success: 'テストユーザー２でログインしました'
   end
 
   def destroy
