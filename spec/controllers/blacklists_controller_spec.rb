b3f17f6cf08e93ace1cbdecd9f6cd2c555723a95
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-12-01 10:05:10 +0900

    delete: コントローラースペックを削除

--- spec/controllers/blacklists_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe BlacklistsController, type: :controller do
-
-end
--- spec/controllers/game_starts_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe GameStartsController, type: :controller do
-
-end
